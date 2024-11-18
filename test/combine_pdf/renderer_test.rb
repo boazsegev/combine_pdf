a30667c8da1221db3baf0e591ba2714de2403b67
Anthony Kaufman <anthony@miriamtech.com>
2024-11-18 11:12:45 -0800

    Fix Renderer errors with --enable-frozen-string-literal
    There were several instances in which adding .b to string literals was
    necessary to not throw this error when --enable-frozen-string-literal
    was enabled. Exposed with tests.

--- test/combine_pdf/renderer_test.rb
+++ test/combine_pdf/renderer_test.rb
@@ -17,4 +17,30 @@ class CombinePDFRendererTest < Minitest::Test
 
     assert_equal(expected, actual)
   end
+
+  def test_object_to_pdf_indirect_reference_id
+    actual = TestRenderer.new.test_object(
+      :indirect_reference_id => 1,
+      :indirect_generation_number => 2
+    )
+    assert_match /^1 2 obj/, actual
+    assert_match /endobj$/, actual
+  end
+
+  def test_object_to_pdf_is_reference_only
+    actual = TestRenderer.new.test_object(
+      :Pages => {
+        :referenced_object => { :Type => :Pages, :Count => 0, :indirect_reference_id => 3 },
+        :is_reference_only => true
+      }
+    )
+    assert_match /Pages 3 0 R/, actual
+  end
+
+  def test_object_to_pdf_raw_stream_content
+    actual = TestRenderer.new.test_object(
+      :raw_stream_content => 'Testing'
+    )
+    assert_match /stream\nTesting\nendstream/, actual
+  end
 end
